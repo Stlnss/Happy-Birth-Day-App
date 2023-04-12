e97835b96fc3b1bc9619504d3d073d98b29f3584
STILL <129733584+Stlnss@users.noreply.github.com>
2023-04-12 11:25:15 +1000

    Add files via upload

---
+++ app/src/main/java/com/still/happybirthday/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.still.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.still.happybirthday.ui.theme.HappyBirthDayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthDayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    BirthDayGreetingWithText(message = "Happy Birth Day John!", from = "Still")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthDayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = message, fontSize = 36.sp)
+        Text(text = from, fontSize = 24.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthDayCardPreview() {
+    HappyBirthDayTheme {
+        BirthDayGreetingWithText(message = "HBD!!", from = "- from STILL")
+    }
+}
+
