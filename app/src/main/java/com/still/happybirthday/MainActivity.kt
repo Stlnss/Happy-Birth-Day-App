1698b2f0871d520439a41fdbca32aec06423d9bf
stlnss <stlnesslv@gmail.com>
2023-04-14 21:14:42 +1000

    Add resources and manage design in detail

--- app/src/main/java/com/still/happybirthday/MainActivity.kt
+++ app/src/main/java/com/still/happybirthday/MainActivity.kt
@@ -3,15 +3,19 @@ package com.still.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.still.happybirthday.ui.theme.HappyBirthDayTheme
 
@@ -25,7 +29,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthDayGreetingWithText(message = "Happy Birth Day John!", from = "Still")
+                    BirthDayGreetingWithImage(
+                        message = stringResource(R.string.happy_birth_day_text), from = getString(
+                            R.string.signature_text
+                        )
+                    )
                 }
             }
         }
@@ -34,9 +42,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthDayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = message, fontSize = 36.sp)
-        Text(text = from, fontSize = 24.sp)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp, end = 16.dp)
+                .align(Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun BirthDayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+        BirthDayGreetingWithText(message = message, from = from)
     }
 }
 
@@ -44,7 +75,11 @@ fun BirthDayGreetingWithText(message: String, from: String, modifier: Modifier =
 @Composable
 fun BirthDayCardPreview() {
     HappyBirthDayTheme {
-        BirthDayGreetingWithText(message = "HBD!!", from = "- from STILL")
+        BirthDayGreetingWithImage(
+            message = stringResource(R.string.happy_birth_day_text), from = stringResource(
+                id = R.string.signature_text
+            )
+        )
     }
 }
 
